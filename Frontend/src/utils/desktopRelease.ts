dfeff1368a949f0c5efe187175a30a785a650224
Horacio Hudson <hhudsonpc@yahoo.com.br>
2026-08-05 16:18:47 -0300

    chore(desktop): advance release target to 0.1.2
    Align the desktop shell version and web download URL with the new GitHub release so users download the updated installer.

--- Frontend/src/utils/desktopRelease.ts
+++ Frontend/src/utils/desktopRelease.ts
@@ -1,4 +1,4 @@
-export const DESKTOP_RELEASE_TAG = 'geolimites-desktop-v0.1.1';
+export const DESKTOP_RELEASE_TAG = 'geolimites-desktop-v0.1.2';
 
 export const DESKTOP_SETUP_DOWNLOAD_URL =
-  `https://github.com/horaciohudson/geo-limites/releases/download/${DESKTOP_RELEASE_TAG}/GeoLimites-Desktop-Setup-0.1.1-x64.exe`;
+  `https://github.com/horaciohudson/geo-limites/releases/download/${DESKTOP_RELEASE_TAG}/GeoLimites-Desktop-Setup-0.1.2-x64.exe`;
